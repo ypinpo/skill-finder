863c62db8fb4eae2c5ac55351802f159309c073a
ypinpo <1332950@qq.com>
2026-07-10 19:46:33 +0800

    Delete tmp directory

--- tmp/skill_debug.py
+++
@@ -1,6 +0,0 @@
-import sys, json, urllib.request
-
-url = "https://skillsmp.com/api/v1/skills/search?q=agent+self+improvement&sortBy=stars&limit=5"
-resp = urllib.request.urlopen(url, timeout=15)
-data = json.loads(resp.read())
-print(json.dumps(data, indent=2, ensure_ascii=False)[:2000])
